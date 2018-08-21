239d3669f035360ed38b67c8837301e01f3f4e43
WangShaoBin <18210423261@.com>
2018-08-21 16:25:56 +0800

    swagger2

--- src/main/java/com/app/controller/common/IndexController.java
+++ src/main/java/com/app/controller/common/IndexController.java
@@ -1,5 +1,8 @@
 package com.app.controller.common;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
